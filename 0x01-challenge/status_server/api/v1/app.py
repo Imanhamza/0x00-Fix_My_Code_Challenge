6bc9c9357fe40934d745a7a60e6871c5755c79b3
Iman Hamza <iman.ali.hamza@gmail.com>
2023-07-09 21:04:04 +0000

    merge from another version

--- 0x01-challenge/status_server/api/v1/app.py
+++ 0x01-challenge/status_server/api/v1/app.py
@@ -14,7 +14,6 @@ def not_found(error):
     """ json 404 page """
     return make_response(jsonify({"error": "Not found"}), 404)
 
-
 if __name__ == "__main__":
     # python -m api.v1.app 
     app.run(host="0.0.0.0", port=5000)
